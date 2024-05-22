706984897455943ba83ec89a6bffbf66e122accc
Kaan Güneyli <kaanguneyli@Kaan-MacBook-Pro.local>
2024-05-22 04:41:44 +0300

    top comment on delete is left only

--- src/functions.py
+++ src/functions.py
@@ -1,21 +1,89 @@
 import os
 
-def create_type(type_name: str, number_of_fields: int, primary_key_order: int, fields: list[str]):
+PAGE_SIZE = 10
+FILE_SIZE = 10
+OUTPUT_FILE = "output.txt"
+
+
+def create_type(type_name: str, number_of_fields: str, primary_key_order: str, fields: dict):
     # create a folder under files (file), create its page
     if (os.path.isdir(f"files/{type_name}")):
-        print("Type already exists")
-        return
+        #print("Type already exists")
+        return False
     os.mkdir(f"files/{type_name}")
-    pass
+    with open(f"files/{type_name}/metadata.txt", "w") as file:
+        file.write(f"{number_of_fields}\n")
+        file.write(f"{primary_key_order}\n")
+#        for field, field_type in fields.items():
+#            file.write(f"{field} {field_type}\n")
+    for i in range(FILE_SIZE):
+        with open(f"files/{type_name}/page{i}.txt", "w") as file:
+            pass
+    return True
 
+
+
+# FILE DA DOLDUYSA NE YAPILACAK BİLMİYORUM
 def create_record(type_name: str, values: list[str]):
     # find the folder under files (file), find the correct space in txt's (page) and place the record
-    pass
+    if not os.path.isdir(f"files/{type_name}"):
+        #print("Type does not exist")
+        return False
+    with open(f"files/{type_name}/metadata.txt", "r") as file:
+        number_of_fields = int(file.readline().strip())
+        primary_key_order = int(file.readline().strip())
+    if len(values) != number_of_fields:
+        #print("Invalid number of fields")
+        return False
+    for i in range(FILE_SIZE):
+        with open(f"files/{type_name}/page{i}.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                if line.split()[primary_key_order-1] == values[primary_key_order-1]:
+                    #print("Primary key already exists")
+                    return False
+            if len(lines) < PAGE_SIZE:
+                with open(f"files/{type_name}/page{i}.txt", "a") as file:
+                    file.write(" ".join(values) + "\n")
+                    return True
+    return False
+        
 
+# DELETEDEN SONRA SON FILEIN SON ÖGESİNİ BURAYA TAŞI
 def delete(type_name: str, primary_key: str):
     # find the folder under files (file), find the correct space in txt's (page) and delete the record
-    pass
+    if not os.path.isdir(f"files/{type_name}"):
+        #print("Type does not exist")
+        return False
+    with open(f"files/{type_name}/metadata.txt", "r") as file:
+        file.readline()
+        primary_key_order = int(file.readline().strip())
+    for i in range(FILE_SIZE):
+        with open(f"files/{type_name}/page{i}.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                if line.split()[primary_key_order-1] == primary_key:
+                    lines.remove(line)
+                    with open(f"files/{type_name}/page{i}.txt", "w") as file:
+                        file.writelines(lines)
+                    return True
+    return False
+
 
 def search(type_name: str, primary_key: str):
     # find the folder under files (file), find the correct space in txt's (page) and return the record
-    pass
+    if not os.path.isdir(f"files/{type_name}"):
+        #print("Type does not exist")
+        return False
+    with open(f"files/{type_name}/metadata.txt", "r") as file:
+        file.readline()
+        primary_key_order = int(file.readline().strip())
+    for i in range(FILE_SIZE):
+        with open(f"files/{type_name}/page{i}.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                if line.split()[primary_key_order-1] == primary_key:
+                    with open(OUTPUT_FILE, "a") as output:
+                        output.write(line)
+                    return True
+    return False
--- src/main.py
+++ src/main.py
@@ -1,20 +1,78 @@
-PAGE_SIZE = 1
-FILE_SIZE = 1
-MAX_FIELDS = 1
-MAX_TYPE_LENGTH = 1
-MAX_FIELD_LENGTH = 1
+import functions
+import os
+import time
+import shutil
 
+MAX_FIELDS = 6
+MAX_TYPE_LENGTH = 12
+MAX_FIELD_LENGTH = 20
+
+
+def log(time, input, result):
+    if not os.path.exists("log.csv"):
+        with open("log.csv", "a") as file:
+            file.write(f"{time}, {input.strip()}, {result}\n")
+            return
+    with open("log.csv", "a") as file:
+        file.write(f"{time}, {input.strip()}, {result}\n")
+        return
 
 with open("input.txt", "r") as file:
-    lines = [line.strip().split(" ") for line in file.readlines()]
+    lines = [line for line in file.readlines()]
     for line in lines:
-        if line[0] == "create":
-            if line[1] == "type":
-                pass
-            elif line[1] == "record":
-                pass
-        elif line[0] == "delete":
-            pass
-        elif line[0] == "search":
-            pass
-    print(lines)
+        line_lst = line.strip().split(" ") 
+        if line_lst[0] == "create":
+            if line_lst[1] == "type":
+                #try:
+                if len(line_lst) != int(line_lst[3]) * 2 + 5:
+                    #print("Invalid number of arguments: ", len(line_lst), int(line_lst[3]) * 2 + 5)
+                    log(int(time.time()), line, "failure")
+                elif len(line_lst[2]) > MAX_TYPE_LENGTH:
+                    #print("Invalid type name")
+                    log(int(time.time()), line, "failure")
+                else:
+                    fields = {}
+                    for i in range(5, len(line_lst), 2):
+                        if len(line_lst[i]) > MAX_FIELD_LENGTH:
+                            #print("Invalid field name")
+                            log(int(time.time()), line, "failure")
+                            break
+                        fields[line_lst[i]] = line_lst[i + 1]
+                    res = functions.create_type(line_lst[2], line_lst[3], line_lst[4], fields)
+                    if res:
+                        log(int(time.time()), line, "success")
+                    else:
+                        log(int(time.time()), line, "failure")
+                #except:
+                #    print("ERROR 1")
+                #    log(int(time.time()), line, "failure")
+            elif line_lst[1] == "record":
+                if len(line_lst) > MAX_FIELDS + 3:
+                    print("Invalid number of arguments")
+                    log(int(time.time()), line, "failure")
+                else:
+                    res = functions.create_record(line_lst[2], line_lst[3:])
+                    if res:
+                        log(int(time.time()), line, "success")
+                    else:
+                        log(int(time.time()), line, "failure")
+        elif line_lst[0] == "delete":
+            if len(line_lst) != 4:
+                print("Invalid number of arguments")
+                log(int(time.time()), line, "failure")
+            else:
+                res = functions.delete(line_lst[2], line_lst[3])
+                if res:
+                    log(int(time.time()), line, "success")
+                else:
+                    log(int(time.time()), line, "failure")
+        elif line_lst[0] == "search":
+            if len(line_lst) != 4:
+                print("Invalid number of arguments")
+                log(int(time.time()), line, "failure")
+            else:              
+                res = functions.search(line_lst[2], line_lst[3])
+                if res:
+                    log(int(time.time()), line, "success")
+                else:
+                    log(int(time.time()), line, "failure")
